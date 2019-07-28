2243bbd56949972d0d7574b31b51244ca9bf8693
Thr0TT1e <7hr0771e@gmail.com>
2019-07-29 00:34:39 +0300

    5 step

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
-var syntax         = 'sass', // Syntax: sass or scss;
-		gulpVersion    = '4'; // Gulp version: 3 or 4
-		gmWatch        = false; // ON/OFF GraphicsMagick watching "img/_src" folder (true/false). Linux install gm: sudo apt update; sudo apt install graphicsmagick
+var syntax		   = 'sass', // Syntax: sass or scss;
+	gulpVersion    = '4'; // Gulp version: 3 or 4
+	gmWatch        = false; // ON/OFF GraphicsMagick watching "img/_src" folder (true/false). Linux install gm: sudo apt update; sudo apt install graphicsmagick
 
-var gulp          = require('gulp'),
-		gutil         = require('gulp-util' ),
-		sass          = require('gulp-sass'),
-		browserSync   = require('browser-sync'),
-		concat        = require('gulp-concat'),
-		uglify        = require('gulp-uglify'),
-		cleancss      = require('gulp-clean-css'),
-		rename        = require('gulp-rename'),
-		autoprefixer  = require('gulp-autoprefixer'),
-		notify        = require('gulp-notify'),
-		rsync         = require('gulp-rsync'),
-		imageResize   = require('gulp-image-resize'),
-		imagemin      = require('gulp-imagemin'),
-		del           = require('del');
+var gulp			= require('gulp'),
+	gutil			= require('gulp-util' ),
+	sass			= require('gulp-sass'),
+	browserSync		= require('browser-sync'),
+	concat			= require('gulp-concat'),
+	uglify			= require('gulp-uglify'),
+	cleancss		= require('gulp-clean-css'),
+	rename			= require('gulp-rename'),
+	autoprefixer	= require('gulp-autoprefixer'),
+	notify			= require('gulp-notify'),
+	rsync			= require('gulp-rsync'),
+	imageResize		= require('gulp-image-resize'),
+	imagemin		= require('gulp-imagemin'),
+	del				= require('del');
 
 // Local Server
 gulp.task('browser-sync', function() {
